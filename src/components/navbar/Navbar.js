139993c612bb52767a815b3302107991962dbd86
Arushan Sinnadurai <arushan.sinnadurai@gmail.com>
2022-05-12 10:54:50 -0400

    fixing home page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <div>
-                <a href="/">Arushan Sinnadurai</a>
+                <a href="/react-pages">Arushan Sinnadurai</a>
                 <a href="/About">About me</a>
                 <a href="/Contact">Contact</a>
             </div>
