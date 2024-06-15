7b31f5f039652b282e4551c0bb4f76fe64c61c3d
ArushanSinnadurai <arushan.sinnadurai@gmail.com>
2024-06-14 21:07:58 -0400

    fixing profile picture

--- src/components/aboutme/Aboutme.js
+++ src/components/aboutme/Aboutme.js
@@ -1,14 +1,11 @@
 import profilepic from "./profilepic.jpg";
 const About = () => {
   return (
-    <div className="profilepicimage">
-      <img src={profilepic} className="App-logo" alt="logo" width="500" />
-      <div className="p1txt">
-        <p1> Arushan Sinnadurai </p1>
-      </div>
+    <div>
+      <img className="w-21 h-21 rounded-full" src={profilepic} alt="logo" />
 
-      <div className="aboutmetxt">
-        <p1> TXT </p1>
+      <div className=".relative">
+        <p1> Arushan Sinnadurai </p1>
       </div>
     </div>
   );
